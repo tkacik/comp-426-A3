a0118bee8e1e9f97b790262b0b8a0f8353b26f33
T. J. Tkacik <tkacik@gmail.com>
2013-10-02 15:23:03 -0400

    completed functional S-calculator

--- a3.js
+++ a3.js
@@ -8,6 +8,14 @@ Assignment 3 of COMP 426-f13
 'Main' Method
 **************************************/
 
+var thisYear = 2013;
+var yearOfBirth;
+var currentSavings;
+var retirementAge;
+var deathAge;
+var workingYears;
+var retirementYears;
+
 $(document).ready(function () {
 	
 	$('#scenario_builder').on('submit', buildScenario);
@@ -15,6 +23,7 @@ $(document).ready(function () {
 	$('#demographics').on('submit', runScenario);
 	
 	$('#scenario_list').on('click', 'input', null, removeScenario);
+	
 });
 
 var buildScenario = function(e) {
@@ -50,8 +59,45 @@ var removeScenario = function(e) {
 }
 
 var runScenario = function(e) {
-	alert("TODO: RUN");
+	yearOfBirth = $('#year_of_birth').val();
+	currentSavings = $('#current_savings').val();
+	retirementAge = $('#retirement_age').val();
+	deathAge = $('#life_expectancy').val();
+	workingYears = retirementAge - (thisYear - yearOfBirth);
+	retirementYears = deathAge - retirementAge;
+	
+	alert("TODO: VALIDATE DEMOGRAPHICS");
+	$('#scenario_list tbody tr').each(calculateSaveReq);
+	alert("TODO: CREATE TABLE");
 	e.preventDefault();
+}
+
+var calculateSaveReq = function(i, e) {
+	var n = workingYears;
+	var m = retirementYears;
+	var I = currentSavings;
+	var r;
+	var g;
+	var T;
+	$(e).children().each(function(j, f) {
+		switch(j){
+			case 1:
+				r = $(f).text()/100 + 1;
+				break;
+			case 2:
+				g = $(f).text()/100 + 1;
+				break;
+			case 3:
+				T = $(f).text();
+				break;
+			default:
+		};
+	});
+	
+	S = ((T*(1-(Math.pow(g,m)))/(Math.pow(g,m-1))/(1-g))-(I*Math.pow(r,n)))*((1-r)/(1-Math.pow(r,n)));
+	
+	alert(S);
+	//alert("TODO: CALCULATE " + e + i);
 }
 
 //$('#scenario_list').append($("<tr></tr>")).append($("<td></td>").append($('#scenario_builder input[name="scenario_name"]').val()));
